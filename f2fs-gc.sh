49d441a2bcdb99884e50a06ab96daa39a1241f6a
Lucas Lee <lucasleeeeeeeee@gmail.com>
2021-07-24 23:05:19 +0800

    optimize and make the script more adaptable

--- f2fs-gc.sh
+++ f2fs-gc.sh
@@ -1,15 +1,31 @@
 #!/bin/bash
-DRIVE=nvme0n1p3
 DIRTY_THRES=100
-DIRTY_SEGMENTS=$(</sys/fs/f2fs/$DRIVE/dirty_segments)
-while [ $DIRTY_SEGMENTS -gt $DIRTY_THRES ]
-do
-	echo 1 > /sys/fs/f2fs/$DRIVE/gc_urgent_sleep_time
-	echo 1 > /sys/fs/f2fs/$DRIVE/gc_urgent
-	DIRTY_SEGMENTS=$(</sys/fs/f2fs/$DRIVE/dirty_segments)
-	echo $DIRTY_SEGMENTS
-	sleep 5
+for f in /sys/fs/f2fs/*/; do
+
+	# Avoid running the script in the /features folder (obviously)
+	if [[ $f != *"features"* ]]; then
+		echo "Performing GC on $f" 
+		DIRTY_SEGMENTS=$(<${f}dirty_segments)
+
+		# Ensure that we restore the original value
+		original_sleep_time=$(cat "${f}gc_urgent_sleep_time")
+		while [ $DIRTY_SEGMENTS -gt $DIRTY_THRES ]
+		do
+			# Begin GC
+			echo 1 > "${f}gc_urgent_sleep_time"
+			echo 1 > "${f}gc_urgent"
+			DIRTY_SEGMENTS=$(<${f}dirty_segments)
+			echo $DIRTY_SEGMENTS
+
+			# Save some CPU time by invoking sleep
+			sleep 10
+		done
+
+		# Restore the original values and stop gc
+		echo $original_sleep_time > ${f}gc_urgent_sleep_time
+		echo 0 > ${f}gc_urgent
+
+		# Tell the user we stopped GC
+		echo "GC completed for $f"
+	fi
 done
-echo 500 > /sys/fs/f2fs/$DRIVE/gc_urgent_sleep_time
-echo 0 > /sys/fs/f2fs/$DRIVE/gc_urgent
-echo "gc completed!"
